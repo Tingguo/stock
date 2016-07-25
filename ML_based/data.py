f24d3d97854c5afe9bef5c29a3bbade905604444
Tingguo <nalanwutuo@gmail.com>
2016-07-25 17:18:17 +1000

    update

--- ML_based/data.py
+++ ML_based/data.py
@@ -10,7 +10,7 @@ def getStock_C(ticker, start='2005-01-01', end='2015-09-30'):
 	Get daily Chinese stock data from TuShare.
 	"""
 	print ticker
-	df = ts.get_hist_data(str(ticker), index=True, start=start, end=end)
+	df = ts.get_hist_data(str(ticker), start=start, end=end)
 	df = df.iloc[::-1]
 	df = df[['open', 'high', 'low', 'volume', 'close']]
 	df.columns = ['Open', 'High', 'Low', 'Volume', 'AdjClose']
